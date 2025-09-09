1433cc242e58e3748412247f31416ead07fa5b56
Graeme Boyd <graeme@freeagent.com>
2025-09-09 12:32:01 +0100

    More ways to play
    Edinburgh and Remote options

--- app/controllers/who_controller.rb
+++ app/controllers/who_controller.rb
@@ -2,18 +2,45 @@ class WhoController < ApplicationController
   include Authorization
 
   def index
-    @people = Person.kept.with_attached_avatar.count
-    @newbies = Person.kept.newbies.count
+    @everyone = Person.everyone.count
+    @newbies = Person.newbies.count
+    @edinburgh = Person.edinburgh.count
+    @remote = Person.remote.count
+    
+    # Set a new random seed for this session
+    session[:random_seed] = (rand * 2) - 1
+    session[:position] = 0
   end
 
   def play
-    scope = unless params[:recent].present?
-        Person.kept.with_attached_avatar.all
-      else
-        Person.kept.newbies
-      end
+    scope = if params[:recent].present?
+        Person.newbies
+    elsif params[:edinburgh].present?
+      Person.edinburgh
+    elsif params[:remote].present?
+      Person.remote
+    else
+      Person.everyone
+    end
 
-    @person = scope.with_attached_avatar.select {|p| p.avatar.attached?}.sample
+    # Get seed and position from session, with defaults
+    seed = session[:random_seed] || (rand * 2) - 1
+    position = session[:position] || 0
+    
+    # Get total count for this scope to handle wraparound
+    total_count = scope.count
+    
+    # Reset position if we've gone through all records
+    if position >= total_count
+      session[:position] = 0
+      position = 0
+    end
+    
+    # Use the Person#random method to get the next person
+    @person = Person.new.random(scope, seed, position)
+    
+    # Increment position for next call
+    session[:position] = position + 1
 
     names = @person.name.split(" ")
     @masked_name = names.map {|name| name.gsub(/\B[\w]/, "*") }.join(" ")
--- app/models/person.rb
+++ app/models/person.rb
@@ -3,5 +3,18 @@ class Person < ApplicationRecord
 
   has_one_attached :avatar
 
-  scope :newbies, -> { where("joined_date > ?", 1.year.ago) }
+  scope :everyone, -> { kept.with_attached_avatar.joins(:avatar_blob) }
+  scope :newbies, -> { everyone.where("joined_date > ?", 1.year.ago) }
+  scope :edinburgh, -> { everyone.where(location: "Edinburgh") }
+  scope :remote, -> { everyone.where(location: "Remote") }
+
+  def random(scope, seed, position)
+    transaction do
+      ActiveRecord::Base.connection.execute("SET SEED TO #{seed}")
+
+      # Selects the user that is at a pseudo-random position
+      # based on the seed
+      scope.order('RANDOM()').offset(position).first
+    end
+  end
 end
